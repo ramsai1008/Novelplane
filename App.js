56658359f04b416866f3cc62b8ee83a51b7944e3
ramsai1008 <ramsai0014@gmail.com>
2025-06-27 16:25:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+// App.js: Add navbar and routing
 
-import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
+
+import Home from "./pages/index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Reader from "./pages/Reader";
 import Dashboard from "./pages/Dashboard";
 import NovelView from "./pages/NovelView";
+import History from "./pages/History";
 
 export default function App() {
   return (
     <Router>
+      <nav className="bg-gray-800 text-white p-4 flex gap-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/dashboard" className="hover:underline">Dashboard</Link>
+        <Link to="/history" className="hover:underline">History</Link>
+        <Link to="/login" className="ml-auto hover:underline">Login</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -18,6 +27,7 @@ export default function App() {
         <Route path="/read/:novelId/:chapterId" element={<Reader />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/novel/:novelId" element={<NovelView />} />
+        <Route path="/history" element={<History />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
